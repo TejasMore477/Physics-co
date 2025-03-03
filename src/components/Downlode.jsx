3eedd21e4c911306e2dc592a1c342ce71717c191
Tejas More <tejasmore660@gmail.com>
2025-03-03 21:28:51 +0530

    error resoved

--- src/components/Downlode.jsx
+++ src/components/Downlode.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaDownload } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 
 function Downlode() {
   return (
--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -8,7 +8,7 @@ function SecondPage() {
         <Marquee />
         <div className=' px-16 w-full mt-8 h-[60vh] relative flex felx-col items-end justify-between'>
             <div className='flex items-center gap-8 w-full group '>
-                <h2 className='text-[16rem] tracking-tighter group-hover:text-orange-500 cursor-pointer ease-in duration-300'>Dowlode</h2>
+                <h2 className='text-[16rem] tracking-tighter group-hover:text-orange-500 cursor-pointer ease-in duration-300'>Dowload</h2>
                 <div className='size-[16rem] rounded-full bg-orange-500 group-hover:bg-white cursor-pointer ease-in duration-300 grid place-content-center overflow-hidden'>
                   <span className='text-[13rem] text-black group-hover:rotate-[45deg] ease-in duration-300'><LuArrowRight /></span>
                 </div>
